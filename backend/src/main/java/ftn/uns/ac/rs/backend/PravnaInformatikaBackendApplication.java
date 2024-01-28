78e4df60f7300ecbde9e115e93bae103c6338bc2
anaandjelic <ana.andjelic161@gmail.com>
2024-01-28 15:43:10 +0100

    Init backend

---
+++ backend/src/main/java/ftn/uns/ac/rs/backend/PravnaInformatikaBackendApplication.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PravnaInformatikaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PravnaInformatikaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ftn/uns/ac/rs/backend/PravnaInformatikaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PravnaInformatikaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
