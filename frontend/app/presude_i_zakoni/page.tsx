4bba2f3c4d4ff3b558208f2e8168f220bbf372eb
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2024-01-30 02:32:59 +0100

    Add presude i zakoni pdf view

--- frontend/app/presude_i_zakoni/page.tsx
+++ frontend/app/presude_i_zakoni/page.tsx
@@ -1,3 +1,32 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function PresudeIZakoni() {
-  return;
+  return (
+    <div className="flex h-full flex-row overflow-hidden space-x-6">
+      <Tabs defaultValue="presude" className="flex flex-col w-96 h-full">
+        <div className="hack pb-4">
+          <TabsList>
+            <TabsTrigger value="presude">Presude</TabsTrigger>
+            <TabsTrigger value="zakoni">Zakoni</TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value="presude" className="flex-grow overflow-auto">
+          <ScrollArea className="h-full" type="always">
+            {Array.from({ length: 40 }, (_, i) => (
+              <div key={i}>Presuda {i}</div>
+            ))}
+          </ScrollArea>
+        </TabsContent>
+        <TabsContent value="zakoni" className="flex-grow overflow-auto">
+          <ScrollArea className="h-full" type="always">
+            {Array.from({ length: 40 }, (_, i) => (
+              <div key={i}>Zakon {i}</div>
+            ))}
+          </ScrollArea>
+        </TabsContent>
+      </Tabs>
+      <ScrollArea className="w-auto h-full">PDF VIEWER</ScrollArea>
+    </div>
+  );
 }
